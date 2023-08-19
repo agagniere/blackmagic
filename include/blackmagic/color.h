f4c7dca4153a1e7d85bc2b0f529c1e791bc8b8f4
Antoine <antoine.gagniere@orolia2s.com>
2023-08-19 12:14:35 +0200

    Initial: Add libft prepro headers

---
+++ include/blackmagic/color.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "blackmagic/tools.h"
+
+#define NORMAL     0
+#define BOLD       1
+#define DIM        2
+#define ITALIC     3
+#define UNDERLINED 4
+#define NEGATIVE   6
+
+#define BLACK      30
+#define RED        31
+#define GREEN      32
+#define YELLOW     33
+#define BLUE       34
+#define PURPLE     35
+#define CYAN       36
+#define LIGHT_GREY 37
+#define DEFAULT    39
+#define WHITE      97
+
+#define BG_BLACK      40
+#define BG_RED        41
+#define BG_GREEN      42
+#define BG_YELLOW     43
+#define BG_BLUE       44
+#define BG_PURPLE     45
+#define BG_CYAN       46
+#define BG_LIGHT_GREY 47
+#define BG_DEFAULT    49
+#define BG_WHITE      107
+
+#define _COLOR_JOIN(A, B) A;B
+#define COLOR(...) PP_STR(\e[FOLD_RIGHT(_COLOR_JOIN, __VA_ARGS__)m)
---
+++ include/blackmagic/tools.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "private_tools.h"
+#include "private_for.h"
+
+/*
+** Fundamentals
+*/
+
+#define C_ARRAY_LENGTH(A) (sizeof(A) / sizeof(*(A)))
+
+#define PP_STR(M)         _PP_STR(M)
+#define PP_CAT(A, B)      _PP_CAT(A, B)
+#define PP_MAX(A, B)      (A > B ? A : B)
+#define PP_MIN(A, B)      (A < B ? A : B)
+
+#define ARG_COUNT(...)    _ARGCNT1(__VA_ARGS__)
+#define ID(...)           __VA_ARGS__
+#define ADD_COMMA(F, ...) F(__VA_ARGS__),
+
+/*
+** Higher level
+*/
+
+#define EACH(...)         ARG_COUNT(__VA_ARGS__), __VA_ARGS__
+#define FOR(...)          _FOR(__VA_ARGS__)
+
+#define FOLD_RIGHT(F, ...) _FOLDR(F, __VA_ARGS__)
+
+#define CAT(...)          FOLD_RIGHT(PP_CAT, __VA_ARGS__)
+#define MERGE(...)        FOLD_RIGHT(_MERGE, __VA_ARGS__)
+
+/*
+** Numbers manipulation tools
+*/
+
+#define MIN(...) _MINMAX(_LESSER, __VA_ARGS__)
+#define MAX(...) _MINMAX(_GREATER, __VA_ARGS__)
+#define ABS(V)   MAX(V, -(V))
