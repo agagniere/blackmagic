a27e591d611a5215a6182d99f8e70e9cdc303590
Antoine <antoine.gagniere@orolia2s.com>
2023-08-22 17:23:36 +0200

    Finalize pygment lexer

--- extra/pygment/lexer.py
+++ extra/pygment/lexer.py
@@ -1,14 +1,23 @@
-from pygments.lexer import RegexLexer, bygroups
+from pygments.lexer import RegexLexer, bygroups, words, include
 from pygments.token import *
 
 class CustomLexer(RegexLexer):
     name = 'Prepro'
     aliases = ['prepro']
-    filenames = ['*.i']
+    filenames = []
 
     tokens = {
-        'root': [
-            ('defined', Keyword.Reserved),
+        'phase3': [
+            (r'\w+', Name),
+            (r'\d[0-9a-zA-Z+-.]*', Number),
+            (r"'[^']+'", String.Char),
+            (r'"[^"]*"', String),
+        ],
+        'detect_comment': [
+            (r'/[*]', Comment.Multiline, 'comment'),
+            (r'//.*?$', Comment.Single),
+        ],
+        'detect_prepro' : [
             (r'^(#)(include)(\s+)([<"][\w/.]+[">])',
              bygroups(Comment.Hashbang,
                       Keyword,
@@ -27,32 +36,41 @@ class CustomLexer(RegexLexer):
                       Whitespace,
                       Name.Function,
                       Punctuation),
-             'param_list'),
-            (r'^(#)(\w*if\w*|el[si][ef])(\s+)(.*)$',
+             ('prepro', 'param_list')),
+            (r'^(#)(\w*if\w*|el[si][ef])(\s+)$',
              bygroups(Comment.Hashbang,
                       Keyword,
-                      Whitespace,
-                      Comment.Preproc)),
+                      Whitespace)),
             (r'^(#)(.*)$',
              bygroups(Comment.Hashbang,
                       Comment.Preproc)),
-
-            (r'\w+', Name),
-            (r'\d[0-9a-zA-Z+-.]*', Number),
-            (r"'[^']+'", String.Char),
-            (r'"[^"]*"', String),
+        ],
+        'root': [
+            include('phase3'),
+            include('detect_comment'),
+            include('detect_prepro'),
+            ('defined', Keyword.Reserved),
         ],
         'param_list': [
-            (',', Punctuation),
+            include('detect_comment'),
             (r'\w+', Name.Variable),
             (r'\s+', Whitespace),
-            (r'...', Operator),
-            (r'[)]', Punctuation, 'prepro')
+            (r',', Punctuation),
+            (r'[.]{3}', Operator),
+            (r'[)]', Punctuation, '#pop')
         ],
         'prepro' : [
-            (r'\(|\)', Punctuation),
-            (r'[#]+', Operator),
+            include('detect_comment'),
+            (r'(\w+)(\s*)([(])', bygroups(Name.Function, Whitespace, Punctuation)),
+            (r'[A-Z_]+\b', Name.Constant),
             (r'\w+', Name),
-            ('\n', Whitespace, '#pop'),
+            (r'[#]+', Operator),
+            (r'[)(,]+', Punctuation),
+            (words(('__VA_ARGS__', '__VA_OPT__'), suffix=r'\b'), Name.Variable.Magic),
+        ],
+        'comment' : [
+            (r'[^*]+', Comment.Multiline),
+            (r'[*]/', Comment.Multiline, '#pop'),
+            (r'[*]', Comment.Multiline),
         ]
     }
