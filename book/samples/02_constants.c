2e7772a2a39ab6d304db2170a03592219bd117b2
Antoine <antoine.gagniere@orolia2s.com>
2024-02-06 20:17:03 +0100

    Improve sample preprocessed view

--- book/samples/02_constants.c
+++ book/samples/02_constants.c
@@ -1,14 +1,16 @@
 #include <stdio.h>  // dprintf
 #include <stdlib.h> // EXIT_*
 #include <string.h> // strchr
-#include <unistd.h> // STDERR_FILENO
+#include <unistd.h> // *_FILENO
 
+/* Reducing the amount of magic numbers */
 int main(int arg_count, char** arg_values)
 {
-	if (strchr(arg_values[1], '-') == NULL)
+	if (arg_count <= 1 || strchr(arg_values[1], '-') == NULL)
 	{
 		dprintf(STDERR_FILENO, "Expected an argument\n");
 		return EXIT_FAILURE;
 	}
+	dprintf(STDOUT_FILENO, "Received argument: %s\n", arg_values[1] + 1);
 	return EXIT_SUCCESS;
 }
