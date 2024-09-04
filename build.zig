4ffb2f557764f5287cd9eba09f44ad9906ffc241
Antoine <angagnie@student.42.fr>
2024-09-04 11:41:36 +0800

    Add zig files

---
+++ build.zig
@@ -0,0 +1,9 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    b.installDirectory(.{
+        .source_dir = b.path("include/blackmagic"),
+        .install_dir = .header,
+        .install_subdir = "blackmagic"
+    });
+}
