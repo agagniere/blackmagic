c04a70e1b3b241e586d0843143abb31fa961bf73
Antoine <antoine.gagniere@orolia2s.com>
2025-04-05 10:58:29 +0200

    prepro for loop: up to 27 elements

--- include/blackmagic/for.h
+++ include/blackmagic/for.h
@@ -74,4 +74,49 @@
 #define FOR_14(A, B, C, D, E, F, G, H, I, J, K, L, N, O, M, ...) \
 	FOR_7(A, B, C, D, E, F, G, M __VA_OPT__(,) __VA_ARGS__) \
 	FOR_7(H, I, J, K, L, N, O, M __VA_OPT__(,) __VA_ARGS__)
+#define FOR_15(A, B, C, D, E, F, G, H, I, J, K, L, N, O, P, M, ...) \
+	FOR_5(A, B, C, D, E, M __VA_OPT__(,) __VA_ARGS__) \
+	FOR_5(F, G, H, I, J, M __VA_OPT__(,) __VA_ARGS__) \
+	FOR_5(K, L, N, O, P, M __VA_OPT__(,) __VA_ARGS__)
+#define FOR_16(A, B, C, D, E, F, G, H, I, J, K, L, N, O, P, Q, M, ...) \
+	FOR_4(A, B, C, D, M __VA_OPT__(,) __VA_ARGS__) \
+	FOR_4(E, F, G, H, M __VA_OPT__(,) __VA_ARGS__) \
+	FOR_4(I, J, K, L, M __VA_OPT__(,) __VA_ARGS__) \
+	FOR_4(N, O, P, Q, M __VA_OPT__(,) __VA_ARGS__)
+#define FOR_17(A, B, C, D, E, F, G, H, I, J, K, L, N, O, P, Q, R, M, ...) \
+	M(__VA_ARGS__ __VA_OPT__(, ) A) \
+	FOR_16(B, C, D, E, F, G, H, I, J, K, L, N, O, P, Q, R, M __VA_OPT__(,) __VA_ARGS__)
+#define FOR_18(A, B, C, D, E, F, G, H, I, J, K, L, N, O, P, Q, R, S, M, ...) \
+	FOR_9(A, B, C, D, E, F, G, H, I, M __VA_OPT__(,) __VA_ARGS__) \
+	FOR_9(J, K, L, N, O, P, Q, R, S, M __VA_OPT__(,) __VA_ARGS__)
+#define FOR_19(A, B, C, D, E, F, G, H, I, J, K, L, N, O, P, Q, R, S, T, M, ...) \
+	M(__VA_ARGS__ __VA_OPT__(, ) A) \
+	FOR_18(B, C, D, E, F, G, H, I, J, K, L, N, O, P, Q, R, S, T, M __VA_OPT__(,) __VA_ARGS__)
+#define FOR_20(A, B, C, D, E, F, G, H, I, J, K, L, N, O, P, Q, R, S, T, U, M, ...) \
+	FOR_5(A, B, C, D, E, M __VA_OPT__(,) __VA_ARGS__) \
+	FOR_5(F, G, H, I, J, M __VA_OPT__(,) __VA_ARGS__) \
+	FOR_5(K, L, N, O, P, M __VA_OPT__(,) __VA_ARGS__) \
+	FOR_5(Q, R, S, T, U, M __VA_OPT__(,) __VA_ARGS__)
+#define FOR_21(A, B, C, D, E, F, G, H, I, J, K, L, N, O, P, Q, R, S, T, U, V, M, ...) \
+	FOR_7(A, B, C, D, E, F, G, M __VA_OPT__(,) __VA_ARGS__) \
+	FOR_7(H, I, J, K, L, N, O, M __VA_OPT__(,) __VA_ARGS__) \
+	FOR_7(P, Q, R, S, T, U, V, M __VA_OPT__(,) __VA_ARGS__)
+#define FOR_22(A, B, C, D, E, F, G, H, I, J, K, L, N, O, P, Q, R, S, T, U, V, W, M, ...) \
+	FOR_11(A, B, C, D, E, F, G, H, I, J, K, M __VA_OPT__(,) __VA_ARGS__) \
+	FOR_11(L, N, O, P, Q, R, S, T, U, V, W, M __VA_OPT__(,) __VA_ARGS__)
+#define FOR_23(A, B, C, D, E, F, G, H, I, J, K, L, N, O, P, Q, R, S, T, U, V, W, X, M, ...) \
+	M(__VA_ARGS__ __VA_OPT__(, ) A) \
+	FOR_22(B, C, D, E, F, G, H, I, J, K, L, N, O, P, Q, R, S, T, U, V, W, X, M __VA_OPT__(,) __VA_ARGS__)
+#define FOR_24(A, B, C, D, E, F, G, H, I, J, K, L, N, O, P, Q, R, S, T, U, V, W, X, Y, M, ...) \
+	FOR_12(A, B, C, D, E, F, G, H, I, J, K, L, M __VA_OPT__(,) __VA_ARGS__) \
+	FOR_12(N, O, P, Q, R, S, T, U, V, W, X, Y, M __VA_OPT__(,) __VA_ARGS__)
+#define FOR_25(A, B, C, D, E, F, G, H, I, J, K, L, N, O, P, Q, R, S, T, U, V, W, X, Y, Z, M, ...) \
+	M(__VA_ARGS__ __VA_OPT__(, ) A) \
+	FOR_24(B, C, D, E, F, G, H, I, J, K, L, N, O, P, Q, R, S, T, U, V, W, X, Y, Z, M __VA_OPT__(,) __VA_ARGS__)
+#define FOR_26(A, B, C, D, E, F, G, H, I, J, K, L, N, O, P, Q, R, S, T, U, V, W, X, Y, Z, AA, M, ...) \
+	FOR_13(A, B, C, D, E, F, G, H, I, J, K, L, N, M __VA_OPT__(,) __VA_ARGS__) \
+	FOR_13(O, P, Q, R, S, T, U, V, W, X, Y, Z, AA, M __VA_OPT__(,) __VA_ARGS__)
+#define FOR_27(A, B, C, D, E, F, G, H, I, J, K, L, N, O, P, Q, R, S, T, U, V, W, X, Y, Z, AA, AB, M, ...) \
+	M(__VA_ARGS__ __VA_OPT__(, ) A) \
+	FOR_26(B, C, D, E, F, G, H, I, J, K, L, N, O, P, Q, R, S, T, U, V, W, X, Y, Z, AA, AB, M __VA_OPT__(,) __VA_ARGS__)
 ///@endcond
